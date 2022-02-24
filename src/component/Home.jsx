6783880111b43f98295ae6ea98b9ce094906b191
Mihalj97 <mihalj.tandi5@gmail.com>
2022-02-24 16:01:02 +0100

    Products

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import bg from '../assets/bg.png'
+import Products from './Products';
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
                  
               </div>
           </div>
+          <Products />
      </div>
   )
 }
