438ee9c40d35c53dfe7000567ac7cbc8574bab0d
Mihalj97 <mihalj.tandi5@gmail.com>
2022-02-24 16:59:21 +0100

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Navbar from './component/Navbar';
 import './App.css';
 import Home from './component/Home';
+import Products from './component/Products';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
-  );
+    <Router>
+        <Navbar />
+          <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          </Routes>
+    </Router>
+  )
 }
 
 export default App;
